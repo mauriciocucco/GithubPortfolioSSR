c1007f35a5bc4939b541f1e7c0226a9b9221c59d
Mauricio Cucco <mcucco@follett.com>
2022-08-29 22:45:43 -0300

    Preboot added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,16 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 
+import { PrebootModule } from 'preboot';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     CoreModule,
     SharedModule,
-    TransferHttpCacheModule
+    TransferHttpCacheModule,
+    PrebootModule.withConfig({ appRoot: 'app-root' })
   ],
   providers: [],
   bootstrap: [AppComponent]
